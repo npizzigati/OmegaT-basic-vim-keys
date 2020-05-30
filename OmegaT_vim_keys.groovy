1c9500f2b4688778d268ca621157650b81c016cd
Nicholas Pizzigati <npizzigati@gmail.com>
2020-05-30 10:18:32 -0500

    Working spike using KeyListener on pane
    -Use KeyListener on pane instead of keyboard focus manager

--- OmegaT_vim_keys.groovy
+++ OmegaT_vim_keys.groovy
@@ -1,92 +1,192 @@
-// :name=OmegaT Vim Keys :description=Vim Keys for OmegaT
-import java.awt.KeyEventDispatcher;
-import java.awt.KeyboardFocusManager;
+ :name=OmegaT Vim Keys :description=Vim Keys for OmegaT
+
+import java.awt.event.KeyListener;
 import java.awt.event.KeyEvent;
 import org.omegat.gui.editor.IEditor.CaretPosition;
+import org.omegat.gui.editor.EditorController;
+import org.omegat.gui.editor.EditorTextArea3;
 
 class MyNewException extends RuntimeException {
 
-    def MyNewException(){
+    MyNewException(){
         super();
     }
 
-    def MyNewException(String message){
+    MyNewException(String message){
         super(message);
     }
 }
 
-class VirtualSegment {
-    String content;
+class Listener implements KeyListener {
 
-    VirtualSegment(segment) {
-            content = segment; 
+    int keyCharValue;
+    EditorController editor;
+    EditorTextArea3 pane;
+    KeyEvent lastConsumedEvent;
+
+    Listener(EditorController editor, EditorTextArea3 pane) {
+        pane.addKeyListener(this)
+        this.editor = editor;
+        this.pane = pane;  
+        println 'Listener initialized'
+        lastConsumedEvent = null;
+        // this.editor.addKeyListener(this);
     }
 
-    String toString() {
-            return content;
+    void keyPressed(KeyEvent e) {
+        if(isRedispatchedEvent(e)) {
+            return;
+        }
+        e.consume();
     }
 
+    void keyTyped(KeyEvent e) {
+        try {
+            if(isRedispatchedEvent(e)) {
+                return;
+            }
+            lastConsumedEvent = e
+            keyCharValue = (int)e.getKeyChar();
+            println keyCharValue
+            if(e.getKeyChar() == 'q') {
+                throw new MyNewException('Interrupt');
+            }
+            e.consume();
+            pane.dispatchEvent(new KeyEvent(pane, e.getID(), e.getWhen(), e.getModifiers(),
+                                            e.getKeyCode(), e.getKeyChar()));
+        } catch (Exception exc) {
+            println exc.getMessage();
+            pane.removeKeyListener(this);
+        } finally {
+        }
+    }
+
+
+    /** Handle the key-released event from the text field. */
+    void keyReleased(KeyEvent e) {
+        e.consume();
+    }
+
+    boolean isRedispatchedEvent(e) {
+      ((lastConsumedEvent != null) && (lastConsumedEvent.getWhen() == e.getWhen()))
+    }
 }
 
-class Listener {
-    final String ENTER_INSERT_KEY = 'i' 
-    final int ENTER_NORMAL_KEYCODE = 27
+new Listener(editor, editor.editor);
 
-    enum Mode {
-            NORMAL, INSERT
-    }
 
-    KeyEventDispatcher keyDispatcher;
-    def console;
-    def editor;
-    def caret;
-    String currentTrans;
-    String insertion;
-    String keyChar;
-    int keyCode;
-    int currentPos;
-    int positionChange;
-    int eventID;
-    def newPos;
-    String key;
-    Mode mode;
-    KeyboardFocusManager manager = KeyboardFocusManager.getCurrentKeyboardFocusManager();
+    // private void displayInfo(KeyEvent e, String keyStatus){
+        
+    //     //You should only rely on the key char if the event
+    //     //is a key typed event.
+    //     int id = e.getID();
+    //     String keyString;
+    //     if (id == KeyEvent.KEY_TYPED) {
+    //         char c = e.getKeyChar();
+    //         keyString = "key character = '" + c + "'";
+    //     } else {
+    //         int keyCode = e.getKeyCode();
+    //         keyString = "key code = " + keyCode
+    //                 + " ("
+    //                 + KeyEvent.getKeyText(keyCode)
+    //                 + ")";
+    //     }
+        
+    //     int modifiersEx = e.getModifiersEx();
+    //     String modString = "extended modifiers = " + modifiersEx;
+    //     String tmpString = KeyEvent.getModifiersExText(modifiersEx);
+    //     if (tmpString.length() > 0) {
+    //         modString += " (" + tmpString + ")";
+    //     } else {
+    //         modString += " (no extended modifiers)";
+    //     }
+    // }
+// editor.editor.addKeyListener(myListener);
+// myListener.listen();
 
-    Listener(console, editor) {
-        this.console = console
-        this.editor = editor
-        mode = Mode.NORMAL
-    }
+// class MyNewException extends RuntimeException {
 
-    def listen() {
-        keyDispatcher = new KeyEventDispatcher() {
-            boolean dispatchKeyEvent(KeyEvent e) {
-                try {
-                    eventID = e.getID();
+//     def MyNewException(){
+//         super();
+//     }
 
-                    // Is the following block necessary or can we
-                    // just ignore the KEY_RELEASED event?
-                    if(isKeyReleasedEvent(eventID)) {
-                        return true;
-                    } else if(isKeyPressedEvent(eventID)) {
-                        keyCode = e.getKeyCode();
-                        // return true;
-                    }
+//     def MyNewException(String message){
+//         super(message);
+//     }
+// }
 
-                    keyChar = e.getKeyChar();
-                    if(keyChar == KeyEvent.CHAR_UNDEFINED) {
-                        console.println 'Not a Unicode char'
-                        key = KeyEvent.getKeyText(keyCode);
-                    } else {
-                        key = keyChar
-                    }
+// class VirtualSegment {
+//     String content;
 
-                    if(keyChar == 'q') {
-                        throw new MyNewException('Interrupt');
-                    }
+//     VirtualSegment(segment) {
+//             content = segment; 
+//     }
 
-                    console.println "keyCode: $keyCode"
-                    console.println key
+//     String toString() {
+//             return content;
+//     }
+
+// }
+
+// class Listener {
+//     final String ENTER_INSERT_KEY = 'i' 
+//     final int ENTER_NORMAL_KEYCODE = 27
+
+//     enum Mode {
+//             NORMAL, INSERT
+//     }
+
+//     KeyEventDispatcher keyDispatcher;
+//     def console;
+//     def editor;
+//     def caret;
+//     String currentTrans;
+//     String insertion;
+//     String keyChar;
+//     int keyCode;
+//     int currentPos;
+//     int positionChange;
+//     int eventID;
+//     def newPos;
+//     String key;
+//     Mode mode;
+//     KeyboardFocusManager manager = KeyboardFocusManager.getCurrentKeyboardFocusManager();
+
+//     Listener(console, editor) {
+//         this.console = console
+//         this.editor = editor
+//         mode = Mode.NORMAL
+//     }
+
+//     def listen() {
+//         keyDispatcher = new KeyEventDispatcher() {
+//             boolean dispatchKeyEvent(KeyEvent e) {
+//                 try {
+//                     eventID = e.getID();
+
+//                     // Is the following block necessary or can we
+//                     // just ignore the KEY_RELEASED event?
+//                     if(isKeyReleasedEvent(eventID)) {
+//                         return true;
+//                     } else if(isKeyPressedEvent(eventID)) {
+//                         keyCode = e.getKeyCode();
+//                         // return true;
+//                     }
+
+//                     keyChar = e.getKeyChar();
+//                     if(keyChar == KeyEvent.CHAR_UNDEFINED) {
+//                         console.println 'Not a Unicode char'
+//                         key = KeyEvent.getKeyText(keyCode);
+//                     } else {
+//                         key = keyChar
+//                     }
+
+//                     if(keyChar == 'q') {
+//                         throw new MyNewException('Interrupt');
+//                     }
+
+//                     console.println "keyCode: $keyCode"
+//                     console.println key
                     // Pass thru keypresses from backspace and arrow keys
                     // if(isKeyPressedEvent(eventID)) {
                     //     if(isNonEnglishKey(keyChar)) {
@@ -136,44 +236,42 @@ class Listener {
                     // println "This will print ${e.getKeyChar()}"
                     // return false;
 
-                } catch (Exception exc) {
-                    removeDispatcher();
-                    console.println exc.getMessage();
-                } finally {
-                }
-            }
-        };
-        manager.addKeyEventDispatcher(keyDispatcher);
-    }
+                // } catch (Exception exc) {
+                //     removeDispatcher();
+                //     console.println exc.getMessage();
+                // } finally {
+                // }
+//             }
+//         };
+//         manager.addKeyEventDispatcher(keyDispatcher);
+//     }
 
-    def removeDispatcher() {
-        manager.removeKeyEventDispatcher(keyDispatcher)
-    }
+//     def removeDispatcher() {
+//         manager.removeKeyEventDispatcher(keyDispatcher)
+//     }
 
-    def isKeyPressedEvent(eventID) {
-        return eventID == KeyEvent.KEY_PRESSED
-    }
+//     def isKeyPressedEvent(eventID) {
+//         return eventID == KeyEvent.KEY_PRESSED
+//     }
 
-    def isKeyReleasedEvent(eventID) {
-        return eventID == KeyEvent.KEY_RELEASED
-    }
+//     def isKeyReleasedEvent(eventID) {
+//         return eventID == KeyEvent.KEY_RELEASED
+//     }
 
-    def isKeyTypedEvent(eventID) {
-        return eventID == KeyEvent.KEY_TYPED
-    }
+//     def isKeyTypedEvent(eventID) {
+//         return eventID == KeyEvent.KEY_TYPED
+//     }
 
-    def isNonEnglishKey(keyChar) {
-        !('A'..'z').contains(keyChar)
-    }
+//     def isNonEnglishKey(keyChar) {
+//         !('A'..'z').contains(keyChar)
+//     }
 
-    def enterNormalMode() {
-        mode = Mode.NORMAL
-    }
+//     def enterNormalMode() {
+//         mode = Mode.NORMAL
+//     }
 
-    def enterInsertMode() {
-        mode = Mode.INSERT
-    }
-}
+//     def enterInsertMode() {
+//         mode = Mode.INSERT
+//     }
+// }
 
-def myListener = new Listener(console, editor);
-myListener.listen();
