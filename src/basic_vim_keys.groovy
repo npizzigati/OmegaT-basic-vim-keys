d5e25f1898c4dccaf72c2e5601889814326748ea
Nicholas Pizzigati <npizzigati@gmail.com>
2020-06-22 21:26:17 -0500

    Enable remapping to escape key

--- src/basic_vim_keys.groovy
+++ src/basic_vim_keys.groovy
@@ -5,10 +5,10 @@
 
 import java.awt.event.KeyListener;
 import java.awt.event.KeyEvent;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 import javax.swing.JFrame;
 import javax.swing.JEditorPane;
-import javax.swing.PopupFactory;
-import javax.swing.Popup;
 import javax.swing.SwingUtilities;
 import org.omegat.gui.editor.IEditor;
 import org.omegat.gui.editor.EditorTextArea3;
@@ -54,20 +54,17 @@ class Listener implements KeyListener {
 
   void keyPressed(KeyEvent event) {
     if(isRedispatchedEvent(event)) {
-      println 'Redispatched event';
       return; //This will pass on event to pane
     }
 
     lastKeyPressed = event;
-    println "Key press event: $event.keyChar"
-    print event.getKeyCode();
 
     event.consume();
   }
 
   void keyTyped(KeyEvent event) {
     if(isRedispatchedEvent(event)) {
-    return;
+      return;
     }
     lastKeyTyped = event;
     // Since the keyTyped event comes after the keyPressed event
@@ -89,7 +86,11 @@ class Listener implements KeyListener {
 
   boolean isRedispatchedEvent(KeyEvent event) {
     KeyEvent lastConsumedEvent = (event.getID() == KeyEvent.KEY_PRESSED) ? lastKeyPressed : lastKeyTyped;
-    ((lastConsumedEvent != null) && (lastConsumedEvent.getWhen() == event.getWhen()))
+    ((lastConsumedEvent != null) && isSameEvent(event, lastConsumedEvent));
+  }
+
+  boolean isSameEvent(event, lastConsumedEvent) {
+    (lastConsumedEvent.getWhen() == event.getWhen() && (lastConsumedEvent.getKeyChar() == event.getKeyChar()));
   }
 }
 
@@ -101,20 +102,22 @@ class Stroke {
     this.keyPressed = keyPressed;
     this.keyTyped = keyTyped;
   }
-
 }
 
 abstract class Mode {
 
   KeyManager manager;
-  Map<String, String> remaps;
-  Map<String, String> remapCandidates;
-  List<Character> accumulatedKeys;
-  String remapMatch;
+  // userEnteredRemaps is temporarily -- these will be retrieved
+  // from user or configuration file
+  Map userEnteredRemaps;
+  Map remaps;
+  Map remapCandidates;
+  List accumulatedKeys;
   List<Character> redispatchQueue;
   Long lastKeypressTime;
   Thread remapTimeoutThread;
   static final int REMAP_TIMEOUT = 900;
+  static final VK_KEYS = ['<esc>': KeyEvent.VK_ESCAPE, '<bs>': KeyEvent.VK_BACK_SPACE];
 
   enum ModeID {
     NORMAL, INSERT, VISUAL;
@@ -123,9 +126,6 @@ abstract class Mode {
   Mode(KeyManager manager) {
     this.manager = manager;
     resetAccumulatedKeys();
-
-    // Temporary: put remaps into remaps map
-    // These will eventually be entered by the user
     resetRemapCandidates();
     redispatchQueue = [];
   }
@@ -146,19 +146,33 @@ abstract class Mode {
   }
 
   void process(Stroke stroke) {
-    char keyChar = stroke.keyTyped.keyChar;
+    int key;
+    int keyChar;
 
     if (redispatchQueue) {
+      println "executing stroke ${stroke.keyTyped.keyChar}"
       execute(stroke);
       redispatchQueue.pop();
       return;
     }
 
+    println "remaps: $remaps"
+    
     if (isRemapTimeoutThreadRunning())
       remapTimeoutThread.interrupt();
 
-    accumulatedKeys = accumulatedKeys << keyChar;
+    keyChar = stroke.keyTyped.keyChar;
+    if ((32..126).contains(keyChar)) {
+      key = keyChar; 
+    } else {
+      key = stroke.keyPressed.keyCode;
+    }
+    
+    accumulatedKeys = accumulatedKeys << key;
+
+    println "accumulatedKeys: $accumulatedKeys";
     remapCandidates = findRemapCandidates();
+    println "remapCandidates: $remapCandidates"
 
     if (remapCandidates) {
       handlePossibleRemapMatch();
@@ -167,7 +181,7 @@ abstract class Mode {
     }
   }
 
-  void runRemapTimeoutThread() {
+  void startRemapTimeoutThread() {
     lastKeypressTime = System.currentTimeMillis();
     remapTimeoutThread = new RemapTimeoutThread();
     remapTimeoutThread.start();
@@ -182,17 +196,50 @@ abstract class Mode {
   }
 
   void handlePossibleRemapMatch() {
-      runRemapTimeoutThread();
+      startRemapTimeoutThread();
 
-      remapMatch = remaps[accumulatedKeys.join()];
+      def remapMatch = remaps[accumulatedKeys];
       if (remapMatch) {
+        println "remap found";
         resetAccumulatedKeys();
-        redispatchQueue = remapMatch.split('').toList();
+        resetRemapCandidates();
+        remapTimeoutThread.interrupt();
+        redispatchQueue = remapMatch.clone();
         manager.batchRedispatchKeys(redispatchQueue.clone());
       }
   }
 
+  List tokenizeRemapString(String remapString) {
+    Pattern pattern = Pattern.compile("(<\\w+>|\\w)");
+    Matcher matcher = pattern.matcher(remapString);
+    List<String> results = [];
+    while (matcher.find()) {
+      results << translateKey(matcher.group(1));
+    }
+    return results;
+  }
+
+  Map tokenizeUserEnteredRemaps() {
+    Map tokenizedRemaps = [:];
+    userEnteredRemaps.each { k, v ->
+      tokenizedRemaps[tokenizeRemapString(k)] = tokenizeRemapString(v); 
+    }
+    println "tokenizedRemaps: $tokenizedRemaps";
+    return tokenizedRemaps;
+  }
+  
+  int translateKey(key) {
+    return VK_KEYS[key.toLowerCase()] ?: (int)key;
+    // List results = [];
+    // remapMatch.each {
+      // int translatedKey = VK_KEYS[it.toLowerCase()] ?: (int)it;
+      // results << translatedKey;
+    // }
+    // return results;
+  }
+
   void refireNonRemappedKeys() {
+    println "refiring nonremapped key";
     redispatchQueue = accumulatedKeys;
     manager.batchRedispatchKeys(redispatchQueue.clone());
     resetAccumulatedKeys();
@@ -204,12 +251,15 @@ abstract class Mode {
 
   void resetAccumulatedKeys() {
     accumulatedKeys = [];
+    println "accumulatedKeys reset.";
   }
 
-  Map<String, String> findRemapCandidates() {
-    String joinedAccumulatedKeys = accumulatedKeys.join();
+  Map findRemapCandidates() {
     remaps.findAll { k, v ->
-      k.startsWith(joinedAccumulatedKeys);
+      // k.startsWith(accumulatedKeys);
+      int candidate_size = accumulatedKeys.size();
+      int end_idx = candidate_size - 1;
+      k[0..end_idx] == accumulatedKeys;
     }
   }
 
@@ -224,6 +274,8 @@ class NormalMode extends Mode {
 
   NormalMode(KeyManager manager) {
     super(manager);
+    userEnteredRemaps = ['ab': 'ba', 'bc': 'cb'];
+    remaps = tokenizeUserEnteredRemaps();
   }
 
   void execute(Stroke stroke) {
@@ -247,13 +299,12 @@ class InsertMode extends Mode {
 
   InsertMode(KeyManager manager) {
     super(manager);
-    remaps = [ab: 'ba', bc: 'cb'];
+    userEnteredRemaps = ['ab': 'ba', 'bc': 'cb', 'ei': '<Esc>'];
+    remaps = tokenizeUserEnteredRemaps();
   }
 
   void execute(Stroke stroke) {
-    char keyChar = stroke.keyTyped.keyChar;
-
-    if ((int)keyChar == KeyEvent.VK_ESCAPE) {
+    if ((int)stroke.keyTyped.keyChar == KeyEvent.VK_ESCAPE) {
       manager.switchTo(ModeID.NORMAL)
     } else {
       manager.redispatchEvent(stroke.keyTyped);
@@ -264,6 +315,8 @@ class InsertMode extends Mode {
 class VisualMode extends Mode {
   VisualMode(KeyManager manager) {
     super(manager);
+    userEnteredRemaps = ['ab': 'ba', 'bc': 'cb'];
+    remaps = tokenizeUserEnteredRemaps();
   }
 
   void execute(Stroke stroke) {
@@ -352,7 +405,6 @@ class KeyManager {
 
   void moveCaret(int positionChange) {
     int currentPos = editor.getCurrentPositionInEntryTranslation();
-    println "currentPos = $currentPos"
     IEditor.CaretPosition caretPosition = new IEditor.CaretPosition(currentPos +
                                                             positionChange);
     editor.setCaretPosition(caretPosition);
@@ -360,20 +412,34 @@ class KeyManager {
 
   void batchRedispatchKeys(List queue) {
     queue.each {
-      KeyEvent event = createEvent(it as char);
+      // KeyEvent event = createEvent(it as char);
+      KeyEvent event = createEvent(it);
       Thread.sleep(1); // Prevent KeyEvents being created with the same time
       redispatchEvent(event);
     }
   }
 
-  KeyEvent createEvent(char keyChar) {
-    int id = KeyEvent.KEY_TYPED;
+  // KeyEvent createEvent(char keyChar) {
+  KeyEvent createEvent(key) {
+    int id;
+    int keyCode;
+    char keyChar;
+    if ((32..126).contains(key) || [8, 27, 127].contains(key)) {
+      keyChar = (char)key;
+      keyCode = 0;
+      id = KeyEvent.KEY_TYPED;
+    } else {
+      keyChar = KeyEvent.CHAR_UNDEFINED;
+      keyCode = key;
+      id = KeyEvent.KEY_PRESSED;
+    }
     long when = System.currentTimeMillis();
     int modifiers = 0;
-    int keyCode = 0;
     return new KeyEvent(pane, id, when, modifiers,
                         keyCode, keyChar);
   }
+
+
 
   void redispatchEvent(KeyEvent event) {
     pane.dispatchEvent(new KeyEvent(pane, event.getID(),
@@ -389,6 +455,4 @@ if (binding.hasVariable('testing')) {
   testWindow.setup(editor.editor);
 }
 
-println binding.variables
 new Listener(editor, editor.editor);
-
